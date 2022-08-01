d735a35d75ba48bf92d365e82318d034b15afe15
margaritaritik <margaritakuryaeva@gmail.com>
2022-08-01 21:42:22 +0700

    delete buttons .jsx files

--- src/buttons/MyButton.jsx
+++ src/buttons/MyButton.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 
 const MyButton = ({children,...props}) => {
-    const buttonClick = event =>{
-        // const todosOfMade = todos.filter( completed => completed.completed ===true);
-
-    }
-
     return (
         <button {...props}  > {children}</button>
     );
