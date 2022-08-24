8987b0b17f46f5435a4b20e4d9fda66a271f796b
margaritaritik <margaritakuryaeva@gmail.com>
2022-08-24 20:47:20 +0700

    add image

--- src/components/button/ButtonDelete.jsx
+++ src/components/button/ButtonDelete.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './buttonStyle.module.css'
+// import img_btn_delete from '../../../public/fishbowl-withfish.svg'
 
 const ButtonDelete = ({children,...props}) => {
     return (
         <button className={style.btn}  {...props}  >
-            <img src="./Wastebasket.svg" alt="X" className="btn_delete"/>
+            <img src= '/fishbowl-withfish.svg' alt="X" className="btn_delete"/>
         </button>
     );
 };
